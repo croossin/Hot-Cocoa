a341844f26f9fc4a967b720200b5038408c486e7
Chase Roossin <croossin94@gmail.com>
2016-09-01 18:20:52 -0700

    Author api comments

--- server/api/v1/author/author.controller.js
+++ server/api/v1/author/author.controller.js
@@ -1,10 +1,19 @@
 'use strict';
-
+/** 
+ * =============================================================================
+ * Imports
+ * =============================================================================
+ */
 var scraper   = require('../../../helper/scraper.js');
 var formatter = require('../../../helper/formatter.js');
 var config    = require('../../../config.js');
 var Pod  = require('../../../helper/cocoaModel.js');
 
+/** 
+ * =============================================================================
+ * Public Functions
+ * =============================================================================
+ */
 exports.search = function(req, res){
 	
 	Pod.find({'details.author.name': req.params.author})
