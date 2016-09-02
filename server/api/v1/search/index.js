362c61da18176a1eef5b4e457115e8c6e10db4e1
Chase Roossin <croossin94@gmail.com>
2016-09-01 18:11:58 -0700

    Added fuzzy search, might want to change how we do it

--- server/api/v1/search/index.js
+++ server/api/v1/search/index.js
@@ -1,9 +1,20 @@
 'use strict';
 
+/** 
+ * =============================================================================
+ * Imports
+ * =============================================================================
+ */
 var express = require('express');
 var controller = require('./search.controller');
 var router = express.Router();
 
+/** 
+ * =============================================================================
+ * Routes
+ * =============================================================================
+ */
 router.post('/:searchTerm', controller.search);
+router.get('/fuzzySearch/:searchTerm', controller.searchQuery);
 
 module.exports = router;
--- server/api/v1/search/search.controller.js
+++ server/api/v1/search/search.controller.js
@@ -15,10 +15,29 @@ var Pod  = require('../../../helper/cocoaModel.js');
  * =============================================================================
  */
 exports.search = function(req, res){
-	
+
+	//TODO NEED TO FIX FOR BODY REQUESTS
+	// var skipNumber = req.params.fromPage ? req.params.fromPage : 0;
+
 	Pod.find({'details.tags': req.params.searchTerm})
 	.limit(20)
+	.skip(skipNumber)
 	.sort('-details.amountOfVotes')	
+	.then(function(resp){
+		res.send(resp);
+	}, function(err){
+		res.send(err);
+	})
+}
+
+//This is for when the user is typing in search field
+//and we want to prepoulate assumptions
+exports.fuzzySearch = function(req, res){
+
+	var searchTerm = req.params.searchTerm;
+
+	Pod.find({'details.tags': {'$regex': searchTerm, '$options': 'i'}})
+	.limit(10)
 	.then(function(resp){
 		res.send(resp);
 	}, function(err){
