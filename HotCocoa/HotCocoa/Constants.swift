0701394980455ca3eb919d1e3483c12fc0b7c83b
Chase Roossin <croossin94@gmail.com>
2016-10-14 19:45:02 -0700

    Fixed more autolayout

--- HotCocoa/HotCocoa/Constants.swift
+++ HotCocoa/HotCocoa/Constants.swift
@@ -13,11 +13,11 @@ struct Dimensions{
 }
 
 struct Network{
-//    static let MAIN_URL: String = "https://shielded-plateau-69173.herokuapp.com/api/v1"
-    static let MAIN_URL: String = "http://localhost:8081/api/v1"
+    static let MAIN_URL: String = "https://shielded-plateau-69173.herokuapp.com/api/v1"
+//    static let MAIN_URL: String = "http://localhost:8081/api/v1"
 
-//    static let MAIN_URL_FOR_SOCKETS: String = "https://shielded-plateau-69173.herokuapp.com"
-    static let MAIN_URL_FOR_SOCKETS: String = "http://localhost:8081"
+    static let MAIN_URL_FOR_SOCKETS: String = "https://shielded-plateau-69173.herokuapp.com"
+//    static let MAIN_URL_FOR_SOCKETS: String = "http://localhost:8081"
 
 
     struct Routes {
